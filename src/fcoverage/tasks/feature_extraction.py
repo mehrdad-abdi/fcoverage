cf6fdc4c3dbf3fef6cb4344c995223fda296082b
Mehrdad Abdi <mrdd.abdi@gmail.com>
2025-05-10 23:28:28 +0200

    wip

--- src/fcoverage/tasks/feature_extraction.py
+++ src/fcoverage/tasks/feature_extraction.py
@@ -2,6 +2,7 @@ import os
 from .base import TasksBase
 from fcoverage.lib.llm import LLMWrapper
 
+
 class FeatureExtractionTask(TasksBase):
 
     def __init__(self, args, config):
@@ -28,7 +29,11 @@ class FeatureExtractionTask(TasksBase):
         """
         Load the feature extraction prompt from the config.
         """
-        filepath = os.path.join(self.args.project, self.config["prompts-directory"], "feature_extraction.txt")
+        filepath = os.path.join(
+            self.args.project,
+            self.config["prompts-directory"],
+            "feature_extraction.txt",
+        )
         with open(filepath, "r") as file:
             self.feature_extraction_prompt = file.read()
 
@@ -37,8 +42,10 @@ class FeatureExtractionTask(TasksBase):
         Join the documents into a single string for processing.
         """
         return "\n\n".join([f"{doc[0]}\n{doc[1]}" for doc in self.documents])
-        
+
     def run(self):
-        self.llm.invoke({
-            "documents": self.join_documents(),
-        })
+        self.llm.invoke(
+            {
+                "documents": self.join_documents(),
+            }
+        )
