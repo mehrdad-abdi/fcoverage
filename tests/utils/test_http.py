48b2948f4874f67227f6a4d0b8c0957a07a38694
Mehrdad Abdi <mrdd.abdi@gmail.com>
2025-05-11 13:55:21 +0200

    wip

--- tests/utils/test_http.py
+++ tests/utils/test_http.py
@@ -1,4 +1,4 @@
-from fcoverage.lib.http import get_github_repo_details
+from fcoverage.utils.http import get_github_repo_details
 
 def test_get_github_repo_details(mock_requests_get_github):
     result = get_github_repo_details("https://github.com/octocat/Hello-World")
