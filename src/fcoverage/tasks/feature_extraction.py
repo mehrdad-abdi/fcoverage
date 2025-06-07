5b09f6204c9b9219fea377a674108d6b095cf94f
Mehrdad Abdi <mrdd.abdi@gmail.com>
2025-06-07 23:02:31 +0200

    Adds feature extraction acceptance test (#27)
    * added_feature_extraction_test
    
    * working
    
    * black + requirements
    
    * fixed test
    
    * wip

--- src/fcoverage/tasks/feature_extraction.py
+++ src/fcoverage/tasks/feature_extraction.py
@@ -1,5 +1,7 @@
+import json
 import os
 
+from fcoverage.models.feature_list import FeatureItem, ProjectFeatureAnalysis
 from fcoverage.utils import prompts
 from .base import TasksBase
 from langchain.chat_models import init_chat_model
@@ -18,15 +20,17 @@ class FeatureExtractionTask(TasksBase):
     def __init__(self, args, config):
         super().__init__(args, config)
         self.documents = []
-        self.model = init_chat_model(
-            config.get("llm", {}).get("model", "gemini-2.0-flash"),
-            model_provider=config.get("llm", {}).get("provider", "google-genai"),
-        )
+        self.model_name = config.get("llm", {}).get("model", "gemini-2.0-flash")
+        self.model_provider = config.get("llm", {}).get("provider", "google-genai")
         self.prompt_template = None
         self.project_name = "not available"
         self.project_description = "not available"
 
     def prepare(self):
+        self.model = init_chat_model(
+            self.model_name,
+            model_provider=self.model_provider,
+        )
         self.load_documents()
         self.load_feature_extraction_prompt()
         if "github" in self.args:
@@ -66,19 +70,137 @@ class FeatureExtractionTask(TasksBase):
                 "documents": documents,
             }
         )
-        return self.model.invoke(prompt)
+        structured_llm = self.model.with_structured_output(ProjectFeatureAnalysis)
+        return structured_llm.invoke(prompt)
 
-    def write_response_to_file(self, response):
+    def feature_item_to_markdown(self, index: int, feature: FeatureItem):
+        is_unique = ""
+        critical_to_user_experience = ""
+        is_likely_to_change = ""
+        if feature.is_unique:
+            is_unique = "[**unique** feature compared to similar projects]"
+        if feature.critical_to_user_experience:
+            critical_to_user_experience = "[**critical** for user experience]"
+        if feature.is_likely_to_change:
+            is_likely_to_change = "[likely to **evolve** frequently]"
+        edge_cases_to_test = ""
+        if feature.edge_cases_to_test:
+            edge_cases_to_test_items = "\n".join(
+                f"- {e}" for e in feature.edge_cases_to_test
+            )
+            edge_cases_to_test = """#### Edge cases to test:
+
+{edge_cases_to_test_items}   
+""".format(
+                edge_cases_to_test_items=edge_cases_to_test_items
+            )
+
+        return f"""### {index}. {feature.name}
+
+*{feature.description}*
+
+Testing priority for this feature: **{feature.priority}**
+
+{critical_to_user_experience} {is_unique} {is_likely_to_change}
+
+{edge_cases_to_test}
+"""
+
+    def features_to_markdown(self, features: ProjectFeatureAnalysis):
+        primary_goals = "\n".join(
+            f"{idx + 1}. {f}" for idx, f in enumerate(features.main_goals)
+        )
+        expected_features = "\n".join(
+            f"{idx + 1}. {f}" for idx, f in enumerate(features.expected_features)
+        )
+        similar_projects = "\n".join(
+            f"{idx + 1}. {f}" for idx, f in enumerate(features.similar_projects)
+        )
+        features_list_items = []
+        for p in ["High", "Medium", "Low"]:
+            # sort High to Low
+            features_list_items.extend(
+                [
+                    self.feature_item_to_markdown(idx + 1, f)
+                    for idx, f in enumerate(features.features)
+                    if f.priority == p
+                ]
+            )
+        features_list = "\n\n".join(features_list_items)
+        edge_case_notes = ""
+        if features.edge_case_notes:
+            edge_case_notes_items = (
+                "\n".join(
+                    f"{idx + 1}. {f}" for idx, f in enumerate(features.edge_case_notes)
+                )
+                if features.edge_case_notes
+                else ""
+            )
+
+            edge_case_notes = """## Edge case notes
+
+*System-wide or project-level edge cases worth testing.*
+
+{edge_case_notes_items}
+""".format(
+                edge_case_notes_items=edge_case_notes_items
+            )
+
+        return """# Features List
+
+**Project Name:** {project_name}
+
+**Project Description:** {project_description}
+
+## Primary Goals: 
+
+*The main goals or objectives the project is trying to achieve.*
+
+{primary_goals}
+
+### Expected Features (as a User)
+
+*Features that a typical user would expect from the project.*
+
+{expected_features}
+
+### Similar Projects/Products
+
+*Other tools, libraries, or projects that serve a similar purpose.*
+
+{similar_projects}
+
+## Features
+
+{features}
+
+{edge_case_notes}
+
+""".format(
+            project_name=features.project_name,
+            project_description=features.project_description,
+            primary_goals=primary_goals,
+            expected_features=expected_features,
+            similar_projects=similar_projects,
+            features=features_list,
+            edge_case_notes=edge_case_notes,
+        )
+
+    def write_response_to_file(self, features):
         filename = os.path.join(self.args["project"], self.config["feature-file"])
-        with open(
-            filename,
-            "w",
-        ) as file:
-            file.write(response)
+        filename_json = os.path.join(
+            self.args["project"], ".fcoverage", "project-features.json"
+        )
+        with open(filename_json, "w") as file:
+            file.write(json.dumps(features.model_dump(mode="json"), indent=2))
+        print(f"Feature extraction results written to {filename_json}")
+
+        with open(filename, "w") as file:
+            file.write(self.features_to_markdown(features))
         print(f"Feature extraction results written to {filename}")
 
     def run(self):
         response = self.invoke_llm()
-        self.write_response_to_file(response.content)
+        self.write_response_to_file(response)
         print("Feature extraction completed successfully.")
         return True
