53a61564923676d4b1978ae2e4538c226aa4b86f
Mehrdad Abdi <mrdd.abdi@gmail.com>
2025-07-18 23:43:20 +0200

    wip

--- src/fcoverage/tasks/base.py
+++ src/fcoverage/tasks/base.py
@@ -1,10 +1,17 @@
+import os
+from pathlib import Path
+from typing import Dict, List
 from fcoverage.utils import prompts
 from langchain.chat_models import init_chat_model
+
+from fcoverage.utils.vdb import VectorDBHelper
 
 
 class TasksBase:
     def __init__(self, args):
         self.args = args
+        self.model = None
+        self.vdb = None
 
     def prepare(self):
         raise NotImplementedError("Subclasses must implement this method")
@@ -17,9 +24,101 @@ class TasksBase:
 
     def load_llm_model(self):
         model_name = self.args.get("llm-model")
-        model_provider = self.config.get("llm-provider")
+        model_provider = self.args.get("llm-provider")
 
         self.model = init_chat_model(
             model_name,
             model_provider=model_provider,
         )
+
+    def load_vector_db_helper(self):
+        self.vdb = VectorDBHelper(
+            persist_directory=self.args["vector-db-persist"],
+            collection_name="fcoverage",
+            embedding_model=self.args["embedding-model"],
+            embedding_provider=self.args["embedding-provider"],
+        )
+
+    from langchain_core.tools import tool
+
+    @tool
+    def search_vector_db(self, query: str, k: int = 5) -> List[str]:
+        """Search the vector DB for chunks related to a natural language query."""
+        results = self.vdb.search(query, k=k)
+        return [
+            f"[{doc.metadata.get('source')}]\n{doc.page_content}" for doc in results
+        ]
+
+    @tool
+    def load_file_section(self, path: str, start: int, end: int) -> str:
+        """Load specific lines from a file."""
+        try:
+            with open(path, "r") as f:
+                lines = f.readlines()
+            return "".join(lines[start - 1 : end])
+        except Exception as e:
+            return f"Error reading file: {e}"
+
+    @tool
+    def grep_string(
+        self, search: str, page_size: int = 10, page: int = 1
+    ) -> List[Dict[str, str]]:
+        """Search for a string in code files and return matching lines with file name and line number. Supports pagination."""
+        result = []
+        for file in Path(self.args["project"]).rglob("*.py"):
+            try:
+                with open(file, "r", errors="ignore") as f:
+                    for lineno, line in enumerate(f, start=1):
+                        if search in line:
+                            result.append(
+                                {
+                                    "file": str(file),
+                                    "line": lineno,
+                                    "text": line.strip(),
+                                }
+                            )
+            except Exception as e:
+                result.append(
+                    {"file": str(file), "line": -1, "text": f"Error reading file: {e}"}
+                )
+
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+        return result[start_index:end_index]
+
+    @tool
+    def list_directory(self, path: str) -> List[Dict[str, str]]:
+        """List files and folders in a directory with metadata (type file or dir, size in kb if it's file, children count if a dir)."""
+
+        path_abs = os.path.join(self.args["project"], path)
+        path_obj = Path(path_abs)
+        if not path_obj.exists():
+            return [{"error": f"Path '{path}' does not exist."}]
+        if not path_obj.is_dir():
+            return [{"error": f"Path '{path}' is not a directory."}]
+
+        results = []
+        for item in path_obj.iterdir():
+            if item.is_file():
+                size_kb = round(item.stat().st_size / 1024, 2)
+                results.append(
+                    {"name": item.name, "type": "file", "size": f"{size_kb} KB"}
+                )
+            elif item.is_dir():
+                num_files = sum(1 for f in item.iterdir() if f.is_file())
+                num_dirs = sum(1 for f in item.iterdir() if f.is_dir())
+                results.append(
+                    {
+                        "name": item.name,
+                        "type": "dir",
+                        "children": f"{num_files} file(s), {num_dirs} dir(s)",
+                    }
+                )
+            else:
+                results.append(
+                    {
+                        "name": item.name,
+                        "type": "other",
+                    }
+                )
+        return results
--- src/fcoverage/tasks/feature_catalog.py
+++ src/fcoverage/tasks/feature_catalog.py
@@ -1,11 +1,16 @@
 import json
 import os
-from typing import List, Literal
+import time
+from typing import List, Literal, Set
 from pydantic import BaseModel, Field
+from tqdm import tqdm
+from fcoverage.utils.code.pytest_utils import get_test_files, list_available_fixtures
 from fcoverage.utils.prompts import escape_markdown
+from fcoverage.utils.vdb import index_all_project
 from .base import TasksBase
 from fcoverage.utils.http import get_github_repo_details
 from langchain_core.prompts import PromptTemplate
+from langchain.agents import create_tool_calling_agent, AgentExecutor
 
 
 class FeatureItem(BaseModel):
@@ -26,6 +31,12 @@ class FeatureItem(BaseModel):
         description="Testing priority for this feature: High, Medium, or Low.",
         enum=["High", "Medium", "Low"],
     )
+    related_test_files: List[str] = Field(
+        description="This field will be used to store the list of test files related to this feature. Ignore it."
+    )
+    core_code_files: List[str] = Field(
+        description="This field will be used to store the list of core source code files related to this feature. Ignore it."
+    )
 
 
 class ProjectFeatures(BaseModel):
@@ -40,18 +51,29 @@ class ProjectFeatures(BaseModel):
     )
 
 
+class TestToFeatures(BaseModel):
+    related_features: List[str] = Field(
+        description="The list of feature names (exact name according the List of features) that the test tries to cover. Leave it empty if you couldn't relate the test to any feature."
+    )
+
+
 class FeatureCatalogTask(TasksBase):
 
     def __init__(self, args, config):
         super().__init__(args, config)
+        self.project_name = "not available"
+        self.project_description = "not available"
 
     def prepare(self):
         self.load_llm_model()
+        self.load_vector_db_helper()
 
     def run(self):
-        documents = self.load_documents()
-        prompt = self.load_feature_extraction_prompt(documents)
-        features_list = self.invoke_llm(prompt)
+        features_list = self.extract_features()
+        self.index_source_code()
+        self.enrich_with_code_files(features_list)
+        self.enrich_with_test_files(features_list)
+
         out = self.args["out"]
         if not out:
             out = "feature-catalog.json"
@@ -79,22 +101,107 @@ class FeatureCatalogTask(TasksBase):
             self.load_prompt("feature_extraction.txt")
         )
 
-        project_name = "not available"
-        project_description = "not available"
         if "github" in self.args:
             github_details = get_github_repo_details(self.args["github"])
-            project_name = github_details["repo_name"]
-            project_description = github_details["description"]
+            self.project_name = github_details["repo_name"]
+            self.project_description = github_details["description"]
 
         prompt_feature_extraction = feature_extraction_prompt_template.invoke(
             {
-                "project_name": project_name,
-                "project_description": project_description,
+                "project_name": self.project_name,
+                "project_description": self.project_description,
                 "documents": documents,
             }
         )
         return prompt_feature_extraction
 
-    def invoke_llm(self, prompt):
+    def extract_features(self):
+        documents = self.load_documents()
+        prompt = self.load_feature_extraction_prompt(documents)
+
         structured_llm = self.model.with_structured_output(ProjectFeatures)
         return structured_llm.invoke(prompt)
+
+    def index_source_code(self):
+        index_all_project(
+            self.vdb,
+            self.args["project"],
+            "**/*.py",
+            [".py"],
+            batch_size=250,
+            sleep_seconds=1,
+        )
+
+    def enrich_with_test_files(self, features_list: ProjectFeatures):
+        test_folder = os.path.join(self.args["project"], self.args["test-path"])
+
+        for test_file in tqdm(get_test_files(test_folder)):
+            relation = self.realte_test_file_to_features(test_file)
+            for feature in features_list.features:
+                if feature.name in relation.related_features:
+                    if feature.related_test_files is None:
+                        feature.related_test_files = []
+                    if test_file not in feature.related_test_files:
+                        feature.related_test_files.append(test_file)
+
+            time.sleep(4)  # respect rate-limit
+
+    def realte_test_file_to_features(
+        self, test_path: str, features_list, verbose=False
+    ) -> TestToFeatures:
+        test_to_feature_prompt_template = self.load_prompt("test_to_feature.txt")
+        tools = [
+            self.search_vector_db,
+            self.grep_string,
+            self.load_file_section,
+        ]
+        agent = create_tool_calling_agent(
+            llm=self.model,
+            tools=tools,
+            prompt=PromptTemplate.from_template(test_to_feature_prompt_template),
+        )
+        agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=verbose)
+
+        with open(test_path, "r") as f:
+            test_code = f.read()
+        test_fixtures = list_available_fixtures(
+            os.path.abspath(self.args["project"]), test_path
+        )
+
+        response = agent_executor.invoke(
+            {
+                "project_name": self.project_name,
+                "project_description": self.project_description,
+                "test_code": test_code,
+                "test_fixtures": test_fixtures,
+                "features_list": features_list,
+                "filename": test_path,
+            }
+        )
+
+        result = response["output"]
+        structured_llm = self.model.with_structured_output(TestToFeatures)
+        test_to_features = structured_llm.invoke(result)
+        return test_to_features
+
+    def look_up_by_keywords_and_grep(self, keywords: List[str]) -> Set[str]:
+        output = set()
+        for keyword in keywords:
+            result = self.grep_string(keyword)
+            for item in result:
+                output.add(item["file"])
+        return output
+
+    def look_up_by_vector_db(self, queries: List[str]) -> Set[str]:
+        output = set()
+        for query in queries:
+            results = self.vdb.search(query, k=5)
+            for item in results:
+                output.add(item.metadata.get("source"))
+        return output
+
+    def enrich_with_code_files(self, features_list: ProjectFeatures):
+        for feature in features_list.features:
+            files_1 = self.look_up_by_keywords_and_grep(feature.keywords)
+            files_2 = self.look_up_by_vector_db(feature.queries)
+            feature.core_code_files = list(files_1.union(files_2))
