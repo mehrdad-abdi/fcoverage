318bcfa8776317ea4cbb2a79cdab5f422c896a2e
Mehrdad Abdi <mrdd.abdi@gmail.com>
2025-07-20 19:38:15 +0200

    vdb sync

--- src/fcoverage/utils/vdb.py
+++ src/fcoverage/utils/vdb.py
@@ -6,6 +6,7 @@ from langchain.schema import Document
 from langchain_community.document_loaders.generic import GenericLoader
 from langchain_community.document_loaders.parsers import LanguageParser
 from tqdm import tqdm
+import hashlib
 
 
 class VectorDBHelper:
@@ -59,6 +60,29 @@ class VectorDBHelper:
     def get_retriever(self):
         return self.vectorstore.as_retriever()
 
+    def sync_documents(self, documents: List[Document]):
+        current_ids = [d.metadata["id"] for d in documents]
+
+        # 1. Get all existing IDs from the DB
+        existing_ids = self.vectorstore.get()["ids"]
+
+        # 2. Identify what to delete and what to add
+        ids_to_add = [id_ for id_ in current_ids if id_ not in existing_ids]
+        ids_to_delete = [id_ for id_ in existing_ids if id_ not in current_ids]
+
+        # 3. Delete obsolete entries
+        if ids_to_delete:
+            self.vectorstore.delete(ids=ids_to_delete)
+
+        # 4. Add new entries
+        if ids_to_add:
+            docs_to_add = []
+            for doc in documents:
+                if doc.metadata["id"] in ids_to_add:
+                    docs_to_add.append(doc)
+
+            self.add_documents(docs_to_add)
+
 
 def index_all_project(
     vdb: VectorDBHelper, project_root, glob, suffixes, batch_size=250, sleep_seconds=1
@@ -70,8 +94,10 @@ def index_all_project(
         parser=LanguageParser(),
     )
     docs = loader.load()
+    for doc in docs:
+        doc.metadata["id"] = hashlib.sha1(doc.page_content.encode("utf-8")).hexdigest()
 
     for i in tqdm(range(0, len(docs), batch_size)):
         batch = docs[i : i + batch_size]
-        vdb.add_documents(batch)
+        vdb.sync_documents(batch)
         time.sleep(sleep_seconds)
